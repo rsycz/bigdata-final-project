0c647d303c5731e5e9fba080638137b62c2cd8f0
Roman Sycz <rsycz75@cmc.edu>
2026-05-05 15:58:31 -0700

    added index route and templates

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template, request
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
@@ -10,8 +10,25 @@ def get_db():
     return conn
 
 @app.route("/")
-def hello_world():
-    return "Twitter clone coming soon!"
+def index():
+    page = request.args.get('page', 1, type=int)
+    offset = (page - 1) * 20
+
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT users.username, tweets.message, tweets.created_at
+        FROM tweets
+        JOIN users ON tweets.user_id = users.id
+        ORDER BY tweets.created_at DESC
+        LIMIT 20 OFFSET %s
+    """, (offset,))
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    tweets = [{'username': r[0], 'message': r[1], 'created_at': r[2]} for r in rows]
+    return render_template('index.html', tweets=tweets, page=page)
 
 @app.route("/static/<path:filename>")
 def staticfiles(filename):
